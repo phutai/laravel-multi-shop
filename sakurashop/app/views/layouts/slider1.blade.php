f50c247fd49d4e3f4f3e7ca0899341bb53c3e4c3
Eraoni <tuyenvx2212@gmail.com>
2015-08-03 00:20:28 +0700

    commit slider

---
+++ sakurashop/app/views/layouts/slider1.blade.php
@@ -0,0 +1,47 @@
+<div class="col-md-12 sale-product">
+  <h2>Thương hiệu nổi bật</h2>
+  <div class="owl-carousel owl-carousel5">
+    <div>
+      <div class="product-item">
+        <div class="pi-img-wrapper">
+          <img src="../../image/slider/t1.jpg" class="img-responsive" alt="Berry Lace Dress" style="margin:auto">
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="product-item">
+        <div class="pi-img-wrapper">
+          <img src="../../image/slider/t2.jpg" class="img-responsive" alt="Berry Lace Dress" style="margin:auto">
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="product-item">
+        <div class="pi-img-wrapper">
+          <img src="../../image/slider/t3.jpg" class="img-responsive" alt="Berry Lace Dress" style="margin:auto">
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="product-item">
+        <div class="pi-img-wrapper">
+          <img src="../../image/slider/t4.jpg" class="img-responsive" alt="Berry Lace Dress" style="margin:auto">
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="product-item">
+        <div class="pi-img-wrapper">
+          <img src="../../image/slider/t5.jpg" class="img-responsive" alt="Berry Lace Dress" style="margin:auto">
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="product-item">
+        <div class="pi-img-wrapper">
+          <img src="../../image/slider/t6.jpg" class="img-responsive" alt="Berry Lace Dress" style="margin:auto">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
